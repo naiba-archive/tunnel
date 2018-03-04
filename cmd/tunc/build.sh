878220d827661a32819904b371f9cc617db11a08
奶爸 <1@5.nu>
2018-03-04 12:43:31 +0800

    [fix] typo

--- cmd/tunc/build.sh
+++ cmd/tunc/build.sh
@@ -5,5 +5,6 @@
 /root/go/bin/gox --osarch="linux/mips"
 /root/go/bin/gox --osarch="windows/386"
 /root/go/bin/gox --osarch="windows/amd64"
-mv ./tuns.go ../
+mv ./tunc.go ../
 mv ./* ../../bin_data/static/clients
+mv ../tunc.go .
