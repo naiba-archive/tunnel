a41c8e433c640d87ef724c891192df30849f8731
奶爸 <1@5.nu>
2018-03-06 09:21:20 +0800

    [op]优化客户端编译脚本

--- cmd/tunc/build.sh
+++ cmd/tunc/build.sh
@@ -6,5 +6,7 @@
 /root/go/bin/gox --osarch="windows/386"
 /root/go/bin/gox --osarch="windows/amd64"
 mv ./tunc.go ../
+mv ./build.sh ../
 mv ./* ../../bin_data/static/clients
+mv ../build.sh .
 mv ../tunc.go .
